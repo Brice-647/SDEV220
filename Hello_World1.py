e7c4cceeadfd2fdd14e25b5e55966e898fd061ce
Brice-647 <bputhoff@ivytech.edu>
2026-01-24 13:36:43 -0500

    Add files via upload

---
+++ Hello_World1.py
@@ -0,0 +1,26 @@
+def python_data_types():
+    """
+    Doctstrings can be created using triple quotes or triple apostrophes.
+    There are also single and double quotes for single-line docstrings.
+
+    Immutable (cannot be changed after creation):
+        int, float, complex
+        bool
+        str
+        tuple
+        frozenset
+        bytes
+
+    Mutable (can be changed after creation):
+        list
+        dict
+        set
+        bytearray
+
+    Mutability matters because mutable objects can be modified in place,
+    while immutable objects require creating a new value.
+    """
+    pass
+
+my_name = "Brice Puthoff"
+print("Hello! My name is " + my_name + ".")
